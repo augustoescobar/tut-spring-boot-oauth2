609046daf23e7a747194339e8b849c2dcaac8f17
Bruno Bastos <brunobastosg21@gmail.com>
2018-06-02 10:40:34 -0300

    login by form and mysql

---
+++ user-form-auth-server/src/main/java/com/example/SocialApplication.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example;
+
+import com.example.domain.User;
+import com.example.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class
+SocialApplication {
+
+	/*
+	DOCKER para o MYSQL
+	docker run -it --rm --name mysql -p 3306:3306 -v {pwd}/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=uhul mysql:5.7
+	 */
+
+	@RequestMapping({ "/user", "/me" })
+	public Map<String, String> user(Principal principal) {
+		Map<String, String> map = new LinkedHashMap<>();
+		map.put("name", principal.getName());
+		return map;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+		return (evt) -> {
+
+			User admin = userRepository.findOneByUsername("admin");
+			if (admin == null) {
+				admin = new User();
+				admin.setUsername("admin");
+				admin.setPassword(passwordEncoder.encode("wololo"));
+				admin.setEnabled(true);
+				userRepository.save(admin);
+			}
+		};
+	}
+}
